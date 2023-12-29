36716b0ca5410d8ff08796896587dfe4ce1f3557
Камилла К. 01-16 <miramkuanyshev@gmail.com>
2023-12-29 22:48:32 +0300

    последние изменения

--- bot/middleware/start.command.js
+++ bot/middleware/start.command.js
@@ -3,26 +3,25 @@ const { bot } = require("../connections/token.connection");
 const saveUser = require("../common/sequalize/saveUser");
 const { Sequelize } = require("sequelize");
 const db = require("../connections/db.connection");
-require ('../middleware/stage/stage');
+require('../middleware/stage/stage');
 
 
 
 module.exports = bot.start(async (ctx) => {
-   try {
-      await db.authenticate();
-     // Присваиваем значение переменным
-     const login = String(ctx.chat.id);
-     const username = ctx.chat.username ?? "anon";
-     const firstname = ctx.update.message.chat.first_name;
-     // сохраняем пользователя в базу данных
-     await saveUser(login, username, firstname);
-     // отправляем приветственное сообщение
-     await ctx.replyWithPhoto({ source: 'bot/img/1.jpg' }, { caption: `✋🏼 Привет ${firstname}! \nМеня зовут Айгуль, сейчас я расскажу тебе о том как начать свой бизнес с нуля без вложений! \n \n Начну через 3... 2... 1...`});
-     // оправляем отбивку и входим в главное меню
-     await new Promise((resolve) => setTimeout(resolve, 3000));
-     await ctx.reply("Начинаем 🔥");
+   try {git 
+      // Присваиваем значение переменным
+      const login = String(ctx.chat.id);
+      const username = ctx.chat.username ?? "anon";
+      const firstname = ctx.update.message.chat.first_name;
+      // сохраняем пользователя в базу данных
+      await saveUser(login, username, firstname);
+      // отправляем приветственное сообщение
+      await ctx.replyWithPhoto({ source: 'bot/img/1.jpg' }, { caption: `✋🏼 Привет ${firstname}! \nМеня зовут Айгуль, сейчас я расскажу тебе о том как начать свой бизнес с нуля без вложений! \n \n Начну через 3... 2... 1...` });
+      // оправляем отбивку и входим в главное меню
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      await ctx.reply("Начинаем 🔥");
    } catch {
-    await ctx.reply("Что-то пошло не так")
+      await ctx.reply("Что-то пошло не так")
    }
    await new Promise((resolve) => setTimeout(resolve, 1000));
    await ctx.scene.enter("menuScene");
